331ca4acb7b2145f789eccbabbb1d853e668779f
qui2k3 <6251071074@st.utc2.edu.vn>
2025-03-17 13:46:31 +0700

    update onleavehover

--- src/components/movie/Catergory.jsx
+++ src/components/movie/Catergory.jsx
@@ -13,24 +13,23 @@ const Catergory = (props) => {
     <div
       className="relative inline-block text-left"
       onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
-      {/* Nút kích hoạt */}
       <span
         className="inline-block w-[78px] h-[28px] text-center gap-x-1.5 font-semibold shadow-xs outline-none text-[18px] text-white font-sans fw-black text-shadow bg-transparent cursor-pointer"
+        
       >
         {title}
       </span>
-
-      {/* Dropdown */}
       <div
-        className={`absolute grid grid-cols-3 right-0 z-10 mt-2 w-96 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 backdrop-blur-md transition ${
+        className={`absolute bg-transparent -mt-1 grid grid-cols-3 right-0 z-10 w-96 origin-top-right rounded-md  shadow-lg ring-1 ring-black/5 backdrop-blur-md transition ${
           isOpen ? "block" : "hidden"
         }`}
         onMouseLeave={handleMouseLeave}
       >
         {genres.map((genre, index) => (
           <div key={index} className="py-1 cursor-pointer">
-            <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+            <div className="block px-4 py-2 text-sm text-white hover:bg-gray-100 hover:text-gray-900">
               {genre}
             </div>
           </div>
